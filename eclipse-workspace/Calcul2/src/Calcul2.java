30e7d07d463aa10d049db89f1f18eb73a548fe4f
yur2 <coflquf1@naver.com>
2018-02-12 16:32:12 +0900

    연산자만 예외처리했어용

--- eclipse-workspace/Calcul2/src/Calcul2.java
+++ eclipse-workspace/Calcul2/src/Calcul2.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calcul2 {
@@ -11,19 +10,25 @@ public class Calcul2 {
 	public void Start() {
 
 		System.out.println("================================================\n");
-		System.out.println("유 ㄸㅣ의 계산기에영ㅎ \n\n계산이 필요하시면 입력!! 고고~쓍");
+		System.out.println("유띠의 계산기에영ㅎ \n\n계산이 필요하시면 입력!! 고고~쓍");
 		System.out.println("    (단 종료하고 싶을시, 느낌표를 눌러주세요)");
 	}
 
 	public void Operator(int sum) {
 
 		while (true) {
-			 if (value >= -2147483648 && value <= 2147483647) {
 			
-			 } else {
-			 System.out.println("잘못넣었소");
-			 }
+			while (true) {
 
+			if (sum >= -2147483648 && sum <= 2147483647) {
+				
+				break;
+
+			} else {
+				System.out.println("잘못넣었소");
+				sum = scan.nextInt();
+			}
+			}
 			operator = scan.next();
 
 			while (true) {
@@ -44,23 +49,26 @@ public class Calcul2 {
 					System.out.println("끝이오!!");
 					System.out.println("================================================\n");
 					return;
-
 				} else {
 					System.out.println("잘못넣었소 연산자기호를 넣어주세요!");
 					operator = scan.next();
 
-			}
+				}
 			}
 
 			value = scan.nextInt();
 
-			// if (value >= 0x30 && value <= 0x39) {
-			// // 숫자 OK!
-			// break;
-			// } else {
-			// System.out.println("잘못넣었소");
-			// }
+			while (true) {
 
+				if (sum >= -2147483648 && sum <= 2147483647) {
+					
+					break;
+
+				} else {
+					System.out.println("잘못넣었소");
+					sum = scan.nextInt();
+				}
+				}
 			switch (operator) {
 
 			case "+":
@@ -91,5 +99,5 @@ public class Calcul2 {
 		}
 
 	}
-
 }
+
